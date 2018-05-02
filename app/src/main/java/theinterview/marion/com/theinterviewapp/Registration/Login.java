b2913bb5e2c160f73b96e343994737b81d2e1418
Shashwath Kamath <kamathshashwath1991@gmail.com>
2018-05-02 14:59:01 -0400

    Initial commit

---
+++ app/src/main/java/theinterview/marion/com/theinterviewapp/Registration/Login.java
@@ -0,0 +1,15 @@
+package theinterview.marion.com.theinterviewapp.Registration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import theinterview.marion.com.theinterviewapp.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
